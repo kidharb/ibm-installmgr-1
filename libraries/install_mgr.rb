0643359ce8d25e586ce10dffe430216a8c9c57e3
Kidhar Bachan <kidharb@gmail.com>
2017-11-10 10:39:12 +0200

    Run execute blocks as service_user and service_group instead of root (#11)
    [Description]
    1. Made the helper "user" aware, instead of running imcl commands as
    'root'. imcl can only be run as the user it was installed with
    2. Moved user / group creation outside of this cookbook. The user /
    group should be created before using tihs cookbook.

--- libraries/install_mgr.rb
+++ libraries/install_mgr.rb
@@ -42,28 +42,6 @@ module InstallMgrCookbook
     action :install do
       unless package_installed?(new_resource.package_name, "#{new_resource.install_dir}/tools")
 
-        user new_resource.service_user do
-          comment 'ibm installation mgr service account'
-          home "/home/#{new_resource.service_user}"
-          shell '/bin/bash'
-          not_if { new_resource.service_user == 'root' }
-        end
-
-        directory "/home/#{new_resource.service_user}" do
-          owner new_resource.service_user
-          group new_resource.service_user
-          mode '0750'
-          recursive true
-          action :create
-          not_if { new_resource.service_user == 'root' }
-        end
-
-        group new_resource.service_group do
-          members new_resource.service_user
-          append true
-          not_if { new_resource.service_group == 'root' }
-        end
-
         # create required dirs
         dirs = ["#{new_resource.ibm_root_dir}", "#{new_resource.extract_dir}",\
                 "#{new_resource.install_dir}", "#{new_resource.data_location}"]
